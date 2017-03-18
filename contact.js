f64bf537370895d367e052b35e1532b548d37078
Robert Belson <rhbelson@gmail.com>
2017-03-18 17:26:08 -0500

    First commit

---
+++ contact.js
@@ -0,0 +1,28 @@
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(42.051709,-87.671492),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+
+window.onload=function() {
+  var l=document.getElementById("linkedin");
+  l.addEventListener("click",goto_linkedin);
+  var t=document.getElementById("twitter");
+  t.addEventListener("click",goto_twitter);
+  var i=document.getElementById("insta");
+  i.addEventListener("click",goto_insta);
+}
+
+function goto_linkedin() {
+	location.replace("https://www.linkedin.com/in/robert-belson-7886ba126/");
+}
+function goto_twitter() {
+	location.replace("https://www.twitter.com/robertbelson");
+}
+
+function goto_insta() {
+	location.replace("https://www.instagram.com/rhbelson");
+}
