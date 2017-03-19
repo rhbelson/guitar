d2a6afed5c53e83e3af5dbec2d375ab05a1cee68
Robert Belson <rhbelson@gmail.com>
2017-03-18 19:42:09 -0500

    bug fixes

--- contact.js
+++ contact.js
@@ -15,7 +15,7 @@ window.onload=function() {
   var i=document.getElementById("insta");
   i.addEventListener("click",goto_insta);
   var sc=document.getElementById("soundcloud");
-  sc.addEventListener("click"),gotosoundcloud);
+  sc.addEventListener("click",gotosoundcloud);
 }
 
 function goto_soundcloud() {
