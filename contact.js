983a684ed1643979c1da365c821f54a583ad66f0
Robert Belson <rhbelson@gmail.com>
2017-03-20 18:51:59 -0500

    added email service

--- contact.js
+++ contact.js
@@ -16,6 +16,8 @@ window.onload=function() {
   i.addEventListener("click",goto_insta);
   var sc=document.getElementById("soundcloud");
   sc.addEventListener("click",goto_soundcloud);
+  var s=document.getElementById("submit");
+  s.addEventListener("click", send_mail);
 }
 
 function goto_soundcloud() {
@@ -31,4 +33,34 @@ function goto_twitter() {
 
 function goto_insta() {
 	location.replace("https://www.instagram.com/rhbelson");
+}
+
+function send_mail() {
+
+  var n=String(document.getElementById("name").value);
+  var e=String(document.getElementById("email").value);
+  var msg=String(document.getElementById("body").value);
+  console.log(n+","+e+","+msg);
+  var service_id = 'gmail';
+  var template_id = 'feedback';
+  var template_params = {
+    name: n,
+    reply_email: e,
+    message: msg
+};
+
+  if (n && e && msg) {
+  // emailjs.send(service_id,template_id,template_params);
+  console.log("You have Submitted Your Message");
+  emailjs.send('gmail','feedback',template_params);
+  document.getElementById("name").value="";
+  document.getElementById("email").value="";
+  document.getElementById("body").value="";
+  alert("Message Submitted");
+
+  }
+  else {
+    alert("Please complete all of the required fields to submit");
+  }
+
 }
