bd49cb2e1570a5736779adf73493dcd06fee1a53
Robert Belson <rhbelson@gmail.com>
2017-06-09 15:20:28 -0400

    changed front page

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload=function() {
+  var p=document.getElementById("pause_button");
+  p.addEventListener("click",pause_music);
+  var mus=document.getElementById("mus");
+}
+
+function pause_music() {
+	mus.remove();
+}
